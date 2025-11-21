61938c6c7862e5f9c5c255d8c514125d6dd832ea
Tom Smeets <tom@tsmeets.nl>
2025-11-21 01:27:53 +0100

    gui: edit mode

--- src/gui.py
+++ src/gui.py
@@ -10,6 +10,7 @@ class RtNode:
     def __init__(self, value: Value, name: str = None, offset: int = 0):
         self.name = name
         self.value = value
+        self.can_edit = False
         self.text = None
         self.children = []
         self.offset = offset
@@ -25,6 +26,7 @@ class RtNode:
         # Add array/struct offset
         self.addr = addr + self.offset
         self.text = None
+        self.can_edit = False
         type: Value = self.value
         while True:
             if type == None:
@@ -49,6 +51,7 @@ class RtNode:
                 self.text = "[]"
                 break
             elif type.tag == ValueTag.BaseType:
+                self.can_edit = True
                 data = client.read_int(self.addr, type.value)
                 if type.name == "char":
                     self.text = f"{str(data)} ({repr(chr(data))})"
@@ -60,6 +63,36 @@ class RtNode:
 
         for c in self.children:
             c.update(client, self.addr)
+
+    def write(self, client: Client, value: str):
+        type: Value = self.value
+        while True:
+            if type == None:
+                break
+            elif type.tag == ValueTag.Variable:
+                type = type.type()
+            elif type.tag == ValueTag.Typedef:
+                type = type.type()
+            elif type.tag == ValueTag.Pointer:
+                break
+            elif type.tag == ValueTag.Enum:
+                break
+            elif type.tag == ValueTag.Struct:
+                break
+            elif type.tag == ValueTag.Array:
+                break
+            elif type.tag == ValueTag.BaseType:
+                try:
+                    if len(value) == 3 and value[0] == "'" and value[2] == "'":
+                        val = ord(value[1])
+                    else:
+                        val = int(value, 0)
+                    data = client.write_int(self.addr, type.value, val)
+                except:
+                    pass
+                break
+            else:
+                break
 
     def expand(self):
         if self.value.tag == ValueTag.Namespace:
@@ -123,12 +156,7 @@ class RtNode:
         return self.value.pretty()
 
     def draw(self, x: int = 0) -> list[(Self, int)]:
-        text = f"{self.name}"
-        # text = f"0x{self.addr:016x} {text}"
-        # text = self.value.pretty()
-        if self.text != None:
-            text = f"{text} = {self.text}"
-        lines = [(self, x, text)]
+        lines = [(self, x)]
         for c in self.children:
             lines += c.draw(x + 1)
         return lines
@@ -150,7 +178,10 @@ class Gui:
         self.scroll = 0
 
         # List of flattend nodes and indentation level
-        self.lines: list[(RtNode, int, str)] = []
+        self.lines: list[(RtNode, int)] = []
+
+        self.edit_mode:bool = False
+        self.edit_text:str = ""
 
     def update(self):
         self.node.update(self.client, self.client.base_address)
@@ -221,14 +252,24 @@ class Gui:
         # Draw info
         y = 0
         cur_node = self.cursor_node()
-        for node, x, text in self.lines[self.scroll :]:
+
+        cursor_pos = (0, 0)
+        for node, x in self.lines[self.scroll:]:
             if y >= size_y:
                 break
 
             indent = x * 4
             if indent >= size_x:
                 indent = size_x - 1
-            text = f"| {text}"
+
+            text = f"| "
+            text += f"0x{node.addr:016x}"
+            text += node.name
+            if self.edit_mode and node == cur_node:
+                text += " = " + self.edit_text
+            elif node.text is not None:
+                text += " = " + node.text
+
 
             # value = node.read_value(self.client)
             # if value:
@@ -239,9 +280,14 @@ class Gui:
 
             if node == cur_node:
                 scr.addstr(y, indent, text, curses.A_REVERSE)
+                cursor_pos = (indent + len(text), y)
             else:
                 scr.addstr(y, indent, text)
             y += 1
+
+        curses.curs_set(self.edit_mode)
+        if self.edit_mode:
+            scr.move(cursor_pos[1], cursor_pos[0])
 
 
 def main():
@@ -254,10 +300,12 @@ def main():
     client = Client()
     client.connect(args.host, args.port, args.symbol)
 
-    gui = Gui(client)
 
     def gui_main(scr):
-        curses.curs_set(0)
+        gui = Gui(client)
+        curses.cbreak()
+        curses.noecho()
+        scr.keypad(True)
         curses.halfdelay(10)
         while True:
             gui.update()
@@ -265,6 +313,7 @@ def main():
             curses.update_lines_cols()
             scr.clear()
             gui.draw(scr)
+
             scr.refresh()
 
             try:
@@ -272,18 +321,40 @@ def main():
             except:
                 k = None
 
-            if k == "q":
-                break
-            elif k == "j":
-                gui.cursor_next()
-            elif k == "k":
-                gui.cursor_prev()
-            elif k == "l":
-                gui.cursor_down()
-            elif k == " ":
-                gui.cursor_toggle()
-            elif k == "h":
-                gui.cursor_up()
+            cursor_node = gui.cursor_node()
+
+            if gui.edit_mode:
+                if k == '\x1b':
+                    gui.edit_mode = False
+                    gui.edit_text = ""
+                elif k == 'KEY_BACKSPACE':
+                    gui.edit_text = gui.edit_text[:-1]
+                elif k == '\n':
+                    gui.edit_mode = False
+                    cursor_node.write(gui.client, gui.edit_text)
+                    gui.edit_text = ""
+                elif k is not None and len(k) == 1:
+                    gui.edit_text += k
+            else:
+                if k == "q":
+                    break
+                elif k == "\x1b":
+                    break
+                elif k == "c" or k == '\n':
+                    if cursor_node.can_edit:
+                        gui.edit_mode = True
+                elif k == "j":
+                    gui.cursor_next()
+                elif k == "k":
+                    gui.cursor_prev()
+                elif k == "l":
+                    gui.cursor_down()
+                elif k == " ":
+                    gui.cursor_toggle()
+                elif k == "h":
+                    gui.cursor_up()
+                elif k == "c":
+                    gui.cursor_next()
 
     curses.wrapper(gui_main)
 
